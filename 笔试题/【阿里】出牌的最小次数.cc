78e4fa75d1bc1fd757bf7426bfa3059342af5876
Xiaocen Dong <t-xidong@microsoft.com>
2020-03-31 21:29:27 +0800

    200331

---
+++ 笔试题/【阿里】出牌的最小次数.cc
@@ -0,0 +1,23 @@
+/*作者：Mogg
+链接：https://www.nowcoder.com/discuss/386873?type=0&order=0&pos=74&page=1
+来源：牛客网
+
+有一叠扑克牌，每张牌介于1和10之间
+
+有四种出牌方法：
+
+    单出1张
+    出2张对子
+    出五张顺子，如12345
+    出三连对子，如112233 
+
+给10个数，表示1-10每种牌有几张，问最少要多少次能出完
+*/
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int leastCardNum(vector<int> &cardNnum)
+{
+}
